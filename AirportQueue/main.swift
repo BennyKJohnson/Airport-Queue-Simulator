66a6e4f9bb916b294b1772a3f460579a05392d72
Benjamin Johnson <bkj986@uowmail.edu.au>
2015-09-10 18:17:03 +1000

    Initial Commit

---
+++ AirportQueue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AirportQueue
+//
+//  Created by Benjamin Johnson on 10/09/2015.
+//  Copyright © 2015 Benjamin Johnson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
